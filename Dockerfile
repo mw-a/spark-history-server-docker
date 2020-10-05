992d1ef71911371792237056ff293e9c429e9ef0
Aliaksandr Sasnouskikh <jahstreetlove@gmail.com>
2020-10-05 22:41:00 +0300

    Upgrade to Spark 3.0.1

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
-ARG SPARK_IMAGE=sasnouskikh/spark:3.0.0_2.12-hadoop_3.2.0_cloud
+ARG SPARK_IMAGE=sasnouskikh/spark:3.0.1_2.12-hadoop_3.2.0_cloud
 FROM ${SPARK_IMAGE}
-
-$SPARK_HOME/jars
 
 ENTRYPOINT ["/opt/entrypoint.sh"]
--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ dir_path=`dirname ${script_path}`
 no_cache="--no-cache"
 
 repo="sasnouskikh/history-server"
-tag="3.0.0_2.12-hadoop_3.2.0_cloud"
+tag="3.0.1_2.12-hadoop_3.2.0_cloud"
 
 ( cd ${dir_path}; docker build . ${no_cache} -t "${repo}:${tag}" )
 docker push "${repo}:${tag}"
